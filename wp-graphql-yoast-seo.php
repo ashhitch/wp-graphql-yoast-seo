073ae483317f988804c536178d0201de242ad338
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2020-10-02 21:10:50 +0100

    building up connections

--- wp-graphql-yoast-seo.php
+++ wp-graphql-yoast-seo.php
@@ -459,8 +459,50 @@ add_action('graphql_init', function () {
             }
           ]);
         endif;
+
+
+        $taxonomies = get_object_taxonomies($post_type, 'objects');
+
+        foreach ($taxonomies as $tax) {
+
+
+          if ($tax->hierarchical) {
+            $name = ucfirst($post_type) . 'To' . ucfirst($tax->name) . 'ConnectionEdge';
+
+            register_graphql_field($name, 'primary',  [
+              'type'        => 'TermNode',
+              'description' => __('The Yoast SEO Primary ' . $tax->name . 'wp-graphql-yoast-seo'),
+              'resolve'     => function ($post, array $args, AppContext $context) {
+                $id = $post['source']->ID;
+
+                $wpseo_primary_term = new WPSEO_Primary_Term($tax->name, $id);
+                $wpseo_primary_term = $wpseo_primary_term->get_primary_term();
+
+                $term = $context->get_loader('term')->load($wpseo_primary_term);
+                // this is needs to return true or false for each one
+                // wp_send_json($term);
+                return  $term;
+              }
+            ]);
+          }
+        }
       }
     }
+
+    // register_graphql_field('PostToCategoryConnectionEdge', 'primary',  [
+    //   'type'        => 'TermNode',
+    //   'description' => __('The Yoast SEO Primary category', 'wp-graphql-yoast-seo'),
+    //   'resolve'     => function ($post, array $args, AppContext $context) {
+    //     $id = $post['source']->ID;
+
+    //     $wpseo_primary_term = new WPSEO_Primary_Term('category', $id);
+    //     $wpseo_primary_term = $wpseo_primary_term->get_primary_term();
+
+    //     $term = $context->get_loader('term')->load($wpseo_primary_term);
+    //     // wp_send_json($term);
+    //     return  $term;
+    //   }
+    // ]);
 
     register_graphql_field('User', 'seo',  [
       'type'        => 'SEOUser',
