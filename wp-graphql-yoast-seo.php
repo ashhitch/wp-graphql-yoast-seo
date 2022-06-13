c0ceb835276b6309ea9a8d1271802e6604b603e0
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2022-06-13 15:24:59 +0100

    version bump

--- wp-graphql-yoast-seo.php
+++ wp-graphql-yoast-seo.php
@@ -8,7 +8,7 @@
  * Author URI:      https://www.ashleyhitchcock.com
  * Text Domain:     wp-graphql-yoast-seo
  * Domain Path:     /languages
- * Version:         4.18.0
+ * Version:         4.18.1
  *
  * @package         WP_Graphql_YOAST_SEO
  */
