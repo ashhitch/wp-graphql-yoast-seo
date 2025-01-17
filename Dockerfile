b73070552e49b3c0742d07b1b2d1bb7761d5a977
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2025-01-17 09:07:10 +0000

    config changes

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,6 @@ RUN docker-php-ext-install mysqli
 RUN curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
   && chmod +x wp-cli.phar \
   && mv wp-cli.phar /usr/local/bin/wp
+
+# Ensure WP-CLI is in the PATH
+ENV PATH="/usr/local/bin:$PATH"
--- wp-config.php
+++ wp-config.php
@@ -2,7 +2,7 @@
 define('DB_NAME', 'exampledb');
 define('DB_USER', 'exampleuser');
 define('DB_PASSWORD', 'examplepass');
-define('DB_HOST', 'db');
+define('DB_HOST', 'db'); // Ensure this matches your Docker service name for the database
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
@@ -10,7 +10,7 @@ $table_prefix = 'wp_';
 
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
-define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_DISPLAY', true);
 
 if (!defined('ABSPATH')) {
     define('ABSPATH', dirname(__FILE__) . '/');
